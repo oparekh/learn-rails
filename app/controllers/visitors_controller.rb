5ccd755c96b7148b77a95c7a15ebe96645b7e284
oparekh <oliver.parekh@gmail.com>
2013-07-21 16:58:59 +0200

    stylesheets

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,5 +3,7 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 		render 'visitors/new'
+		flash.now[:notice] = 'Welcome'
+		flash.now[:alert] = 'My  birthda is soon'
 	end
 end
